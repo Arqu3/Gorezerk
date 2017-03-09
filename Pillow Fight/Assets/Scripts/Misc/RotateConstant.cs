d7e2b0fb7cb93968f6565573f3451c779d25ed78
Ludwig Gustavsson <ludwiggustavsson3@gmail.com>
2017-03-09 01:59:03 +0100

    Project cleanup

---
+++ Pillow Fight/Assets/Scripts/Misc/RotateConstant.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateConstant : MonoBehaviour
+{
+    //Public vars
+    public Vector3 m_RotationAxis = Vector3.zero;
+    public float m_RotationSpeed = 10.0f;
+
+    void Update()
+    {
+        transform.Rotate(m_RotationAxis * m_RotationSpeed * Time.deltaTime);
+    }
+}
